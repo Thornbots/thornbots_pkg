3134d44a3278c4d4b62053de9f12d8e2ee3a976e
Blaise Baptist <blaise.baptist@gmail.com>
2026-06-23 16:09:48 -0400

    added meshes

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf.xacro')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
         (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz*')),
         (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
         (os.path.join('share', package_name, 'config'), glob('config/*')),
