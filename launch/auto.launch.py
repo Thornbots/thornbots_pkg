9e89f6f0aca563d474e64e6a438241d74fd95319
Blaise Baptist <blaise.baptist@gmail.com>
2026-06-30 10:16:27 -0400

    added missing imports

--- launch/auto.launch.py
+++ launch/auto.launch.py
@@ -3,7 +3,14 @@ import xacro
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, AppendEnvironmentVariable
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    AppendEnvironmentVariable,
+    ExecuteProcess,
+    RegisterEventHandler,
+)
+from launch.event_handlers import OnShutdown
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
